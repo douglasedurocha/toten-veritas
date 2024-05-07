12cf6101c197d4814c01328cfa3a51b5c0c00816
Douglas Rocha <douglasedurocha@gmail.com>
2024-05-07 16:21:33 -0300

    feat : new tab 'On sale' appers only if there's a product on sale

--- lib/views/shared/shoppingsection.dart
+++ lib/views/shared/shoppingsection.dart
@@ -17,70 +17,86 @@ class ShoppingSection extends StatefulWidget {
 class _ShoppingSectionState extends State<ShoppingSection>
     with SingleTickerProviderStateMixin {
   late TabController tabController;
-  late List<ContentView> contentViews;
-  late Future<bool> hasSaleProducts;
-
-  FutureBuilder<List<ProductModel>> _buildFutureBuilder(String category) {
-    return FutureBuilder<List<ProductModel>>(
-      future: category == 'sale' ? DBHelper().getProductsOnSale() : DBHelper().getProductsByCategory(category),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return ProductList(products: snapshot.data!);
-        } else if (snapshot.hasError) {
-          return Text('Error: ${snapshot.error}');
-        } else {
-          return const CircularProgressIndicator();
-        }
-      },
-    );
-  }
 
   @override
-  initState() {
+  void initState() {
     super.initState();
-    contentViews = [
-      ContentView(
-        tab: const CustomTab(title: 'Em Promoção', icon: Icons.local_offer),
-        content: _buildFutureBuilder('sale'), 
-      ),
-      ContentView(
-        tab: const CustomTab(title: 'Bebidas', icon: Icons.local_drink),
-        content: _buildFutureBuilder('bebida'), 
-      ),
-      ContentView(
-        tab: const CustomTab(title: 'Salgados', icon: Icons.fastfood),
-        content: _buildFutureBuilder('salgado'),
-      ),
-      ContentView(
-        tab: const CustomTab(title: 'Combos', icon: Icons.local_dining),
-        content: _buildFutureBuilder('combo'),
-      ),
-      ContentView(
-        tab: const CustomTab(title: 'Quitandas', icon: Icons.cake),
-        content: _buildFutureBuilder('doce'),
-      ),
-    ];
-    tabController = TabController(length: contentViews.length, vsync: this);
+    tabController = TabController(length: 5, vsync: this);
   }
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       flex: 16,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          CustomTabBar(
-              controller: tabController,
-              tabs: contentViews.map((e) => e.tab).toList()),
-          Expanded(
-            child: TabBarView(
-              controller: tabController,
-              children: contentViews.map((e) => e.content).toList(),
-            ),
-          )
-        ],
+      child: FutureBuilder(
+        future: Future.wait([
+          DBHelper().getProductsOnSale(),
+          DBHelper().getProductsByCategory('bebida'),
+          DBHelper().getProductsByCategory('salgado'),
+          DBHelper().getProductsByCategory('combo'),
+          DBHelper().getProductsByCategory('doce'),
+        ]),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Container(
+              alignment: Alignment.center,
+              child: const CircularProgressIndicator()
+            );
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            List<ProductModel> productsOnSale = snapshot.data![0];
+            List<ProductModel> bebidaProducts = snapshot.data![1];
+            List<ProductModel> salgadoProducts = snapshot.data![2];
+            List<ProductModel> comboProducts = snapshot.data![3];
+            List<ProductModel> doceProducts = snapshot.data![4];
+
+            var contentViews = [
+              ContentView(
+                tab: const CustomTab(
+                    title: 'Em Promoção', icon: Icons.local_offer),
+                content: ProductList(products: productsOnSale),
+              ),
+              ContentView(
+                tab: const CustomTab(title: 'Bebidas', icon: Icons.local_drink),
+                content: ProductList(products: bebidaProducts),
+              ),
+              ContentView(
+                tab: const CustomTab(title: 'Salgados', icon: Icons.fastfood),
+                content: ProductList(products: salgadoProducts),
+              ),
+              ContentView(
+                tab: const CustomTab(title: 'Combos', icon: Icons.local_dining),
+                content: ProductList(products: comboProducts),
+              ),
+              ContentView(
+                tab: const CustomTab(title: 'Quitandas', icon: Icons.cake),
+                content: ProductList(products: doceProducts),
+              ),
+            ];
+
+            if (productsOnSale.isEmpty) {
+              contentViews.removeAt(0);
+            }
+
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                CustomTabBar(
+                  controller: tabController,
+                  tabs: contentViews.map((e) => e.tab).toList(),
+                ),
+                Expanded(
+                  child: TabBarView(
+                    controller: tabController,
+                    children: contentViews.map((e) => e.content).toList(),
+                  ),
+                ),
+              ],
+            );
+          }
+        },
       ),
     );
   }
